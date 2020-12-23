ba944ca23ef8dd10c1db32d21c637f3819ee9d2d
Shailesh Ladumor <shailesh.infyom@gmail.com>
2020-12-23 12:38:32 +0530

    feat: layout published

--- config/infyom/laravel_generator.php
+++ config/infyom/laravel_generator.php
@@ -93,7 +93,7 @@ return [
     |
     */
 
-    'templates'         => 'adminlte-templates',
+    'templates'         => 'stisla-templates',
 
     /*
     |--------------------------------------------------------------------------
