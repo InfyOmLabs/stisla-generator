5a58d469d5fb292d7eb7f0129a599a4190f53b60
Vishal InfyOm <vishal.ribdiya@infyom.com>
2021-08-28 16:36:37 +0530

    chore: modal generation support added

--- config/app.php
+++ config/app.php
@@ -229,6 +229,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Flash' => Laracasts\Flash\Flash::class,
+        'Datatables' => \Yajra\DataTables\Facades\DataTables::class,
     ],
 
 ];
